7ccefd7d06478f340d9ea7a8fd7c480b7f814d0e
Matt Green <verde.mateo.a@gmail.com>
2020-06-17 18:58:48 -0400

    [React]: Adds ApolloClient to Indexs

--- knocker-react-ui/src/index.js
+++ knocker-react-ui/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+//import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
-ReactDOM.render(
+const client = new ApolloClient({
+  uri: "http://0.0.0.0:3000/dev/knocker-api",
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById("root"));
+
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+); */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

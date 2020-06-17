11a04baa360a50237d918091c0cd747150b4e390
Matt Green <verde.mateo.a@gmail.com>
2020-06-16 23:20:36 -0400

    [React]: Replaces NX React With Basic React App

--- knocker-react-ui/libs/feature-sets/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'feature-sets',
-  preset: '../../jest.config.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/feature-sets'
-};

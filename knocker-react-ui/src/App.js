2e6ba9489f8fe0a1643d7799ddcc730e847638ac
Matt Green <verde.mateo.a@gmail.com>
2020-06-26 01:25:53 -0400

    [Authentication]: Basic Authentication

--- knocker-react-ui/src/App.js
+++ knocker-react-ui/src/App.js
@@ -1,66 +1,79 @@
-import React from "react";
-import "./App.css";
-import Machines from "./Machines/Machines";
-import CreateUser from "./components/users/CreateUsers";
-import Signup from "./Auth/Signup";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import {
-  Nav,
-  Navbar,
-  NavItem,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import React, { Component, Fragment } from "react";
 
-import Header from "./components/header/header";
-export default function App() {
-  return (
-    <Router>
-      <div>
+import "./App.css";
+
+import { Link, withRouter } from "react-router-dom";
+import Routes from "./Routes";
+
+import { Nav, Navbar } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true,
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      if (await Auth.currentSession()) {
+        this.userHasAuthenticated(true);
+      }
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+
+    this.setState({ isAuthenticating: false });
+  }
+
+  userHasAuthenticated = (authenticated) => {
+    this.setState({ isAuthenticated: authenticated });
+  };
+
+  handleLogout = async (event) => {
+    await Auth.signOut();
+
+    this.userHasAuthenticated(false);
+    this.props.history.push("/login");
+  };
+
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated,
+    };
+    return (
+      <div className="App container">
         <Navbar fixed="top" bg="dark" expand="xl">
           <Navbar.Brand href="#home">Knocker</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/machines">Machines</Nav.Link>
             </Nav>
-            <Nav pullRight>
-              <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
+            <Nav pullright>
+              {this.state.isAuthenticated ? (
+                <Nav.Link onClick={this.handleLogout}>Logout</Nav.Link>
+              ) : (
+                <Fragment>
+                  <Nav.Link href="/sign-up">Sign Up</Nav.Link>
+                  <Nav.Link href="/login">Login</Nav.Link>
+                </Fragment>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/machines">
-            <Machines />
-          </Route>
-          <Route path="/sign-up">
-            <Signup />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Routes childProps={childProps} />
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+export default withRouter(App);

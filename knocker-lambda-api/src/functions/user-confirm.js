8defa2175caf0256253abba586dc76e29c02399b
Matt Green <verde.mateo.a@gmail.com>
2020-07-03 13:12:41 -0400

    [User Confirm]: User Created Through Lambda

--- knocker-lambda-api/src/functions/user-confirm.js
+++ knocker-lambda-api/src/functions/user-confirm.js
@@ -1,11 +1,63 @@
-module.exports.main = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event,
-    }),
-  };
+"use strict";
+const AWS = require("aws-sdk");
+const Joi = require("joi");
+const { v4 } = require("uuid");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  callback(null, response);
+const dbParams = {
+  TableName: process.env.PLAYER_TABLE,
+  ProjectionExpression: "id, email,  name, scores, usernames",
 };
+
+const postSchema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().required(),
+  name: Joi.string().required(),
+});
+
+module.exports.main = (event, context, callback) => {
+  const requestBody = JSON.parse(event.body);
+  console.log(requestBody);
+  const { error } = postSchema.validate(requestBody);
+  console.log(error);
+  if (error) return callback(error);
+
+  const { username, email, name } = requestBody;
+
+  addUser(createUser(username, email, name), (err, res) => {
+    if (err) return callback(err);
+    callback(
+      null,
+      makeSuccessResponse({
+        message: `User ${username} has been added`,
+        Id: res.id,
+      })
+    );
+  });
+};
+
+const addUser = (user, callback) => {
+  const userData = {
+    TableName: process.env.PLAYER_TABLE,
+    Item: user,
+  };
+  return dynamoDb.put(userData, callback);
+};
+
+const createUser = (username, email, name) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: v4(),
+    createdAt: timestamp,
+    username,
+    email,
+    name,
+  };
+};
+
+const makeSuccessResponse = (data) => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    data,
+  }),
+});
